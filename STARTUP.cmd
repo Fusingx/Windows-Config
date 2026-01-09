e5c765db4021915f6f05c25f7f5b9073f604cca9
Fusing <fusingx@proton.me>
2026-01-09 13:03:56 +0200

    Add files via upload

---
+++ STARTUP.cmd
@@ -0,0 +1,5 @@
+cd "C:\Users\Sweetwaters Church\Jordan\Scripts\StartUp"
+start "" alacritty -e cmd /c "py Main.py"
+cd "C:\Users\Sweetwaters Church\Jordan\Scripts\Shortcuts"
+start pyw main.py
+exit
